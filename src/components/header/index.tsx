c99138b35d4b5daf541a3a64ca3cf5ff8c3a513d
JulianaSobaJava <julianasobajava23@gmail.com>
2023-05-04 16:43:21 +0100

    :sparkles: last commit

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,29 +1,34 @@
-import type { NextComponentType } from 'next'
-import { BsMoonStarsFill, BsSunFill} from 'react-icons/bs';
+import type { NextComponentType } from "next";
+import { BsMoonStarsFill, BsSunFill } from "react-icons/bs";
 import styles from "./styles.module.scss";
-import { useState } from 'react';
+import { useState } from "react";
 
 const Header: NextComponentType = () => {
+  const [togle, setTogle] = useState(false);
+  return (
+    <header className={styles.headerContainer}>
+      <nav className={styles.headerContent}>
+        <h1 className={styles.logo}>
+          {" "}
+          <span className={styles.span}></span>Juliana Soba
+        </h1>
+        <ul>
+          <li className={styles.active}>Home</li>
+          <li>About</li>
+          <li>Experience</li>
+          <li>Projects</li>
+          <li>Funny</li>
+          <li>Blog</li>
+        </ul>
+        <div
+          className={`${styles.icons} ${togle && styles.lightMode}`}
+          onClick={() => setTogle(!togle)}
+        >
+          <button>{togle ? <BsSunFill /> : <BsMoonStarsFill />}</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-    const [togle, setTogle] = useState(false)
-    return (
-        <header className={styles.headerContainer}>
-            <nav className={styles.headerContent}>
-                <h1 className={styles.logo}> <span className={styles.span}></span>Juliana Soba</h1>
-                <ul>
-                    <li className={styles.active}>Home</li>
-                    <li>Sobre</li>
-                    <li>Experiência</li>
-                    <li>Projectos</li>
-                    <li>Funny</li>
-                    <li>Blog</li>
-                </ul>
-                <div className={`${styles.icons} ${togle && styles.lightMode}`} onClick={()=>setTogle(!togle)}>
-                <button>{togle ? <BsSunFill/> : <BsMoonStarsFill/>}</button>
-                </div>
-            </nav>
-        </header>
-    )
-}
-
-export default Header
+export default Header;
