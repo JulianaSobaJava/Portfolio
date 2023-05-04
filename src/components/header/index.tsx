dd0ac82e32bf642b2bcfd51304e252e952dc19f7
JulianaSobaJava <julianasobajava23@gmail.com>
2023-05-04 16:14:43 +0100

    :construction: add changes on header and add icon theme mode

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,26 @@
 import type { NextComponentType } from 'next'
-import { BsChevronLeft, BsChevronRight, BsSlashLg } from 'react-icons/bs';
+import { BsMoonStarsFill, BsSunFill} from 'react-icons/bs';
 import styles from "./styles.module.scss";
+import { useState } from 'react';
 
 const Header: NextComponentType = () => {
+
+    const [togle, setTogle] = useState(false)
     return (
         <header className={styles.headerContainer}>
             <nav className={styles.headerContent}>
-                <h1 className={styles.logo}><BsChevronLeft />JS <span>/</span><BsChevronRight /></h1>
+                <h1 className={styles.logo}> <span className={styles.span}></span>Juliana Soba</h1>
                 <ul>
                     <li className={styles.active}>Home</li>
                     <li>Sobre</li>
                     <li>Experiência</li>
                     <li>Projectos</li>
                     <li>Funny</li>
+                    <li>Blog</li>
                 </ul>
+                <div className={`${styles.icons} ${togle && styles.lightMode}`} onClick={()=>setTogle(!togle)}>
+                <button>{togle ? <BsSunFill/> : <BsMoonStarsFill/>}</button>
+                </div>
             </nav>
         </header>
     )
