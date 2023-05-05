76bde5403df2d4b65b94899b3c1484b6d208f4eb
JulianaSobaJava <julianasobajava23@gmail.com>
2023-05-05 15:18:00 +0100

    push

---
+++ src/components/about/index.tsx
@@ -0,0 +1,31 @@
+const About = (): JSX.Element => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Here you will find more information about me, what I do, and my current
+        skills mostly in terms of programming and technology
+      </p>
+      <div className="content">
+        <div className="left">
+          <h2>Who Am I?</h2>
+          <p>
+            I'm a Frontend Web Developer with a passion for creating beautiful
+            and functional websites. I also like sharing content related to the
+            stuff that I have learned over the years in Web Development so it
+            can help other people of the Dev Community. Feel free to Connect or
+            Follow me on my Linkedin where I post useful content related to Web
+            Development and Programming I'm open to Job opportunities where I
+            can contribute, learn and grow. If you're looking for a web
+            developer who is dedicated, skilled, and passionate about her work,
+            look no further than Me. Contact today to learn more about my
+            services and to discuss your project
+          </p>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
